ec5aa6552f5e97109153bc06aed82294fe29eddb
zhezhenwang <zhezhenwang@gmail.com>
2019-05-09 16:44:00 -0500

    rm getMinipackage

--- getMinipackage/data-raw/intron_gr.chr21.R
+++
@@ -1,7 +0,0 @@
-##
-## intron_gr dataset
-##
-## Loading intron_gr dataset internally in R from data directory:
-## load(file="data/intron_gr.chr21.rda")
-##
-devtools::use_data(intron_gr, overwrite = TRUE)
